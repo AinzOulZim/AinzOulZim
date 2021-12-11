0347c84dec147f59b2e2f5ac7dc6737a8e9b2d82
AinzOulZim <92850797+AinzOulZim@users.noreply.github.com>
2021-12-11 13:37:04 +0300

    Add files via upload

---
+++ individualnoe1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace individual
+{
+    class Program
+    {
+        static void Main(string[] args)
+            
+        {
+            Console.WriteLine("Введите");
+            Console.WriteLine("Фамилию:");
+            string name1 = Console.ReadLine();
+            Console.WriteLine("Должность(П,С,А):");
+            string job1 = Console.ReadLine();
+            Console.WriteLine("Год рождения:");
+            int birthyear1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Оклад(грн):");
+            double pay1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Фамилию:");
+            string name2 = Console.ReadLine();
+            Console.WriteLine("Должность(П,С,А):");
+            string job2 = Console.ReadLine();
+            Console.WriteLine("Год рождения:");
+            int birthyear2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Оклад(грн):");
+            double pay2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Фамилию:");
+            string name3 = Console.ReadLine();
+            Console.WriteLine("Должность(П,С,А):");
+            string job3= Console.ReadLine();
+            Console.WriteLine("Год рождения:");
+            int birthyear3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Оклад(грн):");
+            double pay3 = Convert.ToDouble(Console.ReadLine());
+
+            // заголовок таблицы
+            Console.WriteLine("----------------------------------------------------------------------|");
+            Console.WriteLine(" Отдел кадров                                                         |");
+            Console.WriteLine("----------------------------------------------------------------------|");
+            // данные
+            Console.WriteLine(" Фамилия      | Должность       | Год рождния         |     Оклад(грн)|");
+            Console.WriteLine("----------------------------------------------------------------------|");
+            Console.WriteLine(" "+name1+"  |  "+job1+"  |  "+birthyear1+"  |  "+pay1+"  |  ");
+            Console.WriteLine("----------------------------------------------------------------------|");
+            Console.WriteLine(" " + name2 + "  |  " + job2 + "  |  " + birthyear2 + "  |  " + pay2 + "  |  ");
+            Console.WriteLine("----------------------------------------------------------------------|");
+            Console.WriteLine(" " + name3 + "  |  " + job3 + "  |  " + birthyear3 + "  |  " + pay3 + "  |  ");
+            Console.WriteLine("----------------------------------------------------------------------|");
+            // примечания
+            Console.WriteLine(" П-преподаватель, С-студент, А-аспирант                               |");
+            Console.WriteLine("----------------------------------------------------------------------|");
+
+
+
+
+
+        }
+    }
+}
---
+++ zadanie9.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите коэффициенты для первого столбца");
+
+            Console.WriteLine("a1 = ");
+            int a1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("a2 = ");
+            int a2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("a3 = ");
+            int a3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите коэффициенты для второго столбца");
+
+            Console.WriteLine("b1 = ");
+            int b1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("b2 = ");
+            int b2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("b3 = ");
+            int b3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите коэффициенты для третьего столбца");
+
+            Console.WriteLine("c1 =");
+            int c1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("c2 =");
+            int c2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("c3 =");
+            int c3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите коэффициенты стоящие после знака равно");
+
+            Console.WriteLine("d1 =");
+            int d1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("d2 =");
+            int d2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("d3 =");
+            int d3 = int.Parse(Console.ReadLine());
+
+            int deltageneral = a1 * b2 * c3 + a2 * c1 * b3 + b1 * c2 * a3 - a3 * b2 * c1 - b1 * c3 * a2 - b3 * c2 * a1;
+
+            int deltax = d1 * b2 * c3 + d2 * c1 * b3 + d3 * b1 * c2 - d3 * b2 * c1 - d2 * b1 * c3 - d1 * b3 * c2;
+
+            int deltay = a1 * d2 * c3 + a2 * c1 * d3 + a3 * d1 * c2 - a3 * d2 * c1 - a1 * d3 * c2 - a2 * d1 * c3;
+
+            int deltaz = a1 * b2 * d3 + a2 * d1 * b3 + a3 * b1 * d2 - a3 * b2 * d1 - a2 * b1 * d3 - a1 * b3 * d2;
+
+            int x = deltax / deltageneral;
+
+            int y = deltay / deltageneral;
+
+            int z = deltaz / deltageneral;
+
+            Console.WriteLine("Ответ:");
+
+            Console.WriteLine("x = " + x);
+            Console.WriteLine("y = " + y);
+            Console.WriteLine("z = " + z);
+        }
+    }
+}
